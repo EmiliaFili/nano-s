46b5187d4b5d780a6752d046816af5632bd6c4af
kaleissin <kaleissin@gmail.com>
2015-01-10 22:29:30 +0100

    Use runtests and also test newer versions

--- runtests.py
+++ runtests.py
@@ -1,48 +1,22 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+
 import sys
+import os
 
-import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nano.tests.settings'
+sys.path.append('.')
+
 from django.conf import settings
-
-APPS = [
-    'nano.activation',
-    'nano.badge',
-    'nano.blog',
-    'nano.chunk',
-    'nano.comments',
-    'nano.countries',
-    'nano.faq',
-    'nano.link',
-    'nano.mark',
-    'nano.privmsg',
-    'nano.tools',
-    'nano.user',
-]
-
-SETTINGS = {
-    'DEBUG': True,
-    'DATABASES': {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-        }
-    },
-    'INSTALLED_APPS': [
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.admin',
-    ] + APPS,
-}
-if django.VERSION[:2] < (1, 6):
-    SETTINGS['INSTALLED_APPS'] += ['discover_runner']
-    SETTINGS['TEST_RUNNER'] = 'discover_runner.DiscoverRunner'
-settings.configure(
-    **SETTINGS
-)
+from django.test.utils import get_runner
 
 if __name__ == '__main__':
-    # MUST be imported *after* settings.configure() has run!
-    from django.test.utils import get_runner
+    try: # Django 1.7+
+        from django import setup
+        setup()
+    except ImportError:
+        pass
 
     options = {
         'verbosity': 0,
@@ -51,7 +25,7 @@ if __name__ == '__main__':
     TestRunner = get_runner(settings)
 
     test_runner = TestRunner(**options)
-    failures = test_runner.run_tests(APPS)
+    failures = test_runner.run_tests(settings.APPS)
 
     if failures:
         sys.exit(bool(failures))
