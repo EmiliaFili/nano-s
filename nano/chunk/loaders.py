2f5e7e3ab433e0269522f30ca896382b65ccbad8
kaleissin <kaleissin@gmail.com>
2015-12-29 21:04:32 +0100

    Fix nano.chunk's template loader

--- nano/chunk/loaders.py
+++ nano/chunk/loaders.py
@@ -4,7 +4,7 @@ Wrapper for loading templates from the filesystem.
 
 from django.apps import apps
 from django.conf import settings
-from django.template.base import TemplateDoesNotExist
+from django.template import TemplateDoesNotExist
 from django.utils._os import safe_join
 
 from django.template.loaders.base import Loader
@@ -14,11 +14,9 @@ class ChunkLoader(Loader):
 
     def load_template_source(self, template_name, template_dirs=None):
         chunk_model = apps.get_model('chunk', 'Chunk')
-        chunk_model_name = chunk_model.__name__
-        template_id = (self.chunk_model_name, template_name)
         try:
-            chunk = self.chunk_model.objects.get(slug=template_name)
-            return (chunk.content, "chunk:%s:%s" % template_id)
-        except self.chunk_model.DoesNotExist:
-            error_msg = "Couldn't find a %s-chunk named %s" % template_id
+            chunk = chunk_model.objects.get(slug=template_name)
+            return (chunk.content, template_name)
+        except chunk_model.DoesNotExist:
+            error_msg = "Couldn't find a chunk named %s" % template_name
             raise TemplateDoesNotExist(error_msg)
