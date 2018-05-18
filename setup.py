34ef0e5d8767e49598fe48167831e34dbd0b9e84
kaleissin <kaleissin@gmail.com>
2018-05-18 12:21:37 +0200

    Make setup.py builds exclude tests

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 README_FILE = open('README.rst')
 try:
@@ -10,46 +9,55 @@ finally:
     README_FILE.close()
 
 exclude = [
+    'nanosite',
+    'tests*',
+    '*tests',
+    '*.pyc',
+    '*.pyo',
+    '__pycache__',
 ]
 
 packages=(
-        'nano',
-        'nano.activation', 
-        'nano.badge', 
-        'nano.blog',
-        'nano.chunk',
-        'nano.comments',
-        'nano.countries',
-        'nano.faq',
-        'nano.mark',
-        'nano.privmsg',
-        'nano.tools',
-        'nano.user',
+    'nano.activation',
+    'nano.badge',
+    'nano.blog',
+    'nano.chunk',
+    'nano.comments',
+    'nano.countries',
+    'nano.faq',
+    'nano.mark',
+    'nano.privmsg',
+    'nano.tools',
+    'nano.user',
 )
 
+
 setup(name='nano',
-        version='0.9.4',
-        packages=find_packages(exclude=exclude),
-        include_package_data=True,
-        zip_safe=False,
-        platforms=['any'],
-        description='Does less! Loosely coupled mini-apps for django.',
-        author_email='kaleissin@gmail.com',
-        author='kaleissin',
-        long_description=long_description,
-        url='https://github.com/kaleissin/django-nano',
-        classifiers=[
-                'Development Status :: 4 - Beta',
-                'Environment :: Web Environment',
-                'Framework :: Django',
-                'Intended Audience :: Developers',
-                'License :: OSI Approved :: MIT License',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 2',
-                'Programming Language :: Python :: 2.7',
-                'Programming Language :: Python :: 3',
-                'Topic :: Software Development :: Libraries :: Application Frameworks',
-                'Topic :: Software Development :: Libraries :: Python Modules',
-        ]
+    version='0.9.4',
+    packages=find_packages(exclude=exclude),
+    include_package_data=True,
+    zip_safe=False,
+    platforms=['any'],
+    description='Does less! Loosely coupled mini-apps for django.',
+    author_email='kaleissin@gmail.com',
+    author='kaleissin',
+    long_description=long_description,
+    url='https://github.com/kaleissin/django-nano',
+    python_requires='>=2.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
